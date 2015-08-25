44b6e606f80752585fc85b665861fae46c737a1b
U-JKAMALU2018\johnk_000 <john.kamalu@gmail.com>
2015-08-25 15:13:47 -0700

    Commit before showing to Nikhil

--- public/javascripts/control_player.js
+++ public/javascripts/control_player.js
@@ -1,7 +1,6 @@
 /*
  * 'globals' to keep track of
  */
- 
 var room
 
  /*
@@ -12,6 +11,7 @@ var searchInput = document.getElementById('query-input')
 var joinButton = document.getElementById('room-button')
 var joinInput = document.getElementById('room-input')
 var statusLabel = document.getElementById('label-status')
+var searchContainer = document.getElementById('video-select-container')
 
 /*
  * This code loads the IFrame Player API code asynchronously
@@ -21,58 +21,12 @@ tag.src = "https://www.youtube.com/iframe_api"
 var firstScriptTag = document.getElementsByTagName('script')[0]
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-/*
- * This code opens the socket namespace connection to the default namespace
- */
-var socket = io.connect()
-
-/*
- * SOCKET EVENT: PLAYER RELOADED
- * prereq should be a leader 
- */
-socket.on('reload player with videoId', function(videoId) {
-	player.loadVideoById({
-		'videoId': videoId,
-	})
-})
-
-/*
- * SOCKET EVENT: ROOM JOINED
- */
-socket.on('room joined with roomName and userRole', function(data) {
-	room = data
-	statusLabel.className = 'label label-success'
-	//statusLabel.innerHTML = 'CONNECTED to ' + data.room_name.toUpperCase() + ' as ' + data.user_role.toUpperCase()
-})
-
-/*
- * SOCKET EVENT: USER ROLE RECEIVED
- */
-
-/*
- * JOINING A ROOM
- */
-joinButton.addEventListener('click', function(event) {
-	roomName = joinInput.value
-	joinInput.value = ''
-	console.log('joine room from client')
-	socket.emit('join room with roomName', roomName)
-})
-
-/*
- * RELOADING THE PLAYER
- */
-searchButton.addEventListener('click', function(event) {
-	videoId = searchInput.value
-	searchInput.value = ''
-	socket.emit('reload player with videoId', videoId)
-})
-
 var player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height: '505px',
 		width: '855px',
+		videoId: 'sng_CdAAw8M',
 		events: {
 			'onReady': onPlayerReady,
 			'onStateChange': onPlayerStateChange
@@ -80,17 +34,78 @@ function onYouTubeIframeAPIReady() {
 	})
 }
 
-function onPlayerReady(event) {
-	
-}
-
-var done = false
-function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-
-	}
-}
-
-function stopVideo() {
+function onPlayerReady(event) {	
 	player.stopVideo()
 }
+
+/*
+ * This code opens the socket namespace connection to the default namespace
+ */
+var socket = io.connect()
+
+/*
+ * ROOM JOIN - CLIENT
+ */
+joinButton.addEventListener('click', function(event) {
+	roomName = joinInput.value
+	joinInput.value = ''
+	socket.emit('JOIN FROM CLIENT', {roomName})
+})
+
+/*
+ * ROOM JOIN - SERVER
+ * INPUT DATA: the entire room JSON plus the user role
+ * TODO: synch with times and state
+ */
+socket.on('JOIN FROM SERVER', function(data) {
+	room = data; console.log(room)
+	statusLabel.className = 'label label-success'
+	statusLabel.innerHTML = 'CONNECTED to ' + data.room_name.toUpperCase() + ' as ' + data.join_as.toUpperCase()
+})
+
+/* 
+ * RELOAD PLAYER - CLIENT
+ */
+searchButton.addEventListener('click', function(event) {
+	videoId = searchInput.value
+	searchInput.value = ''
+	socket.emit('RELOAD FROM CLIENT', {roomName: room.room_name, videoId: videoId})
+})
+  
+/*
+ * RELOAD PLAYER - SERVER
+ * INPUT DATA: video id
+ * TODO:
+ */
+socket.on('RELOAD FROM SERVER', function(data) {
+	player.loadVideoById({
+		'videoId': data.room_video,
+	})
+	player.stopVideo()
+})
+
+/*
+ * PLAYER STATE CHANGE - CLIENT 
+ * TODO: improve case when buffering -- pay attentiont to the else
+ */
+function onPlayerStateChange(event) {
+	var playerState = event.data;
+	//0 end, 1 play, 2 pause, 3 buffer
+	if (playerState == 1 || playerState == 2 || playerState == 3) {
+		socket.emit('PLAYER STATE CHANGE FROM CLIENT', {playerState: playerState, currentTime: player.getCurrentTime()})
+	} else {}
+}
+
+/*
+ * PLAYER STATE CHANGE - SERVER
+ * INPUT DATA: state
+ * TODO:
+ */
+socket.on('PLAYER STATE CHANGE FROM SERVER', function(data) {
+	player.seekTo(data.current_time, true)
+	if (data.player_state == 1) {
+		player.playVideo()
+	} else if (data.player_state == 2) {
+		player.pauseVideo()
+	} else {}
+})
