369bf0d036563ddcb3886926e1d511ef3c273e3f
U-JKAMALU2018\johnk_000 <john.kamalu@gmail.com>
2016-01-24 23:48:04 +0100

    Added a lot more styling to my page index. A lot more to go. Just got the scrolling nav bar to work as well as the nav bars bottom border when at fixed position. Just copied the contact us format from the skeleton help page. In the middle of designing inputs.

--- public/javascripts/basic_events.js
+++ public/javascripts/basic_events.js
@@ -1,12 +1,18 @@
 var nav = $(".navbar");
 var header = $("section.header");
+var navlist = $("#navbar-main");
+
 
 $(window).scroll(function() {
 	if ( $(this).scrollTop() > 150 ) {
 		nav.addClass("navbar-scrolled");
 		header.addClass("header-hidden");
+		navlist.addClass("navbar-list-scrolled");
 	} else {
 		nav.removeClass("navbar-scrolled");
 		header.removeClass("header-hidden");
+		navlist.removeClass("navbar-list-scrolled");
 	}
 });
+
+//https://teamtreehouse.com/community/forum-tip-create-a-sticky-navigation-with-css-and-jquery-2
